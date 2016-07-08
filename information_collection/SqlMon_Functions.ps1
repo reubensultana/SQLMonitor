b59d0829f4ba3573165ca964dce8653caaa510f7
Reuben Sultana <reuben.sultana@gmail.com>
2016-07-08 15:41:00 +0100

    Update SqlMon_Functions.ps1

--- information_collection/SqlMon_Functions.ps1
+++ information_collection/SqlMon_Functions.ps1
@@ -39,109 +39,140 @@ Function Get-ServerInfo() {
 
     Foreach ($Server in $ServerInstances) {
         $ServerName = $Server.ServerName
-        $InstanceName = "$($Server.ServerName),$($Server.SqlTcpPort)"
+        $TcpPort = $Server.SqlTcpPort
+        $InstanceName = "$ServerName,$TcpPort"
         "{0} : Processing server: {1}" -f $(Get-Date -Format "HH:mm:ss"), $ServerName
 
-        Foreach ($script in $scripts) {
-            $scriptname = $scriptroot + $script.ScriptName + ".sql"
-            $tablename = $script.ScriptName
-            $intervalminutes = $script.IntervalMinutes
-            # the script that should be executed, retrieved from the database
-            $executescript = $result.ExecuteScript
+        # test connection
+        $TestConnection = Test-Port -hostname $ServerName -port $TcpPort
+        if ($TestConnection -eq $true) {
+            # test authentication
+            try {
+                $result = Invoke-Sqlcmd2 -ServerInstance $InstanceName -Database master -Query "SELECT CURRENT_TIMESTAMP AS [ServerDateTime];"
+                $ServerDateTime = $result.ServerDateTime
+                $TestAuthentication = $true
+            }
+            catch { 
+                $TestAuthentication = $false
+                Write-Warning "Could not log on to $ServerName on port $TcpPort"
+            }
+        }
+        else {
+            Write-Warning "Network access to $ServerName on port $TcpPort not available"
+        }
 
-            # check that the script file exists
-            if (Test-Path $scriptname -PathType Leaf) {
-                # check when the script was last run and compare to the pre-defined value for how many minutes should have elapsed
-                # this will avoid that say, a script that should run Monthly is run multiple times during the month
-                # the COALESCE function will either return the value of the most recent RecordCreated column for that server OR the value 600,000 (which is more than 1 year in minutes)
-                $sql = "SELECT COALESCE(DATEDIFF(N, MAX([RecordCreated]), CURRENT_TIMESTAMP), 600000) AS [MinutesElapsed] FROM $($ProfileName).$($tablename) WHERE [ServerName] = '$($ServerName)';"
-                $result = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $sql
-                $minuteselapsed = $result.MinutesElapsed
-                $result = $null
+        if (($TestConnection -eq $true) -and ($TestAuthentication -eq $true)) {
+            Foreach ($script in $scripts) {
+                $scriptname = $scriptroot + $script.ScriptName + ".sql"
+                $tablename = $script.ScriptName
+                $intervalminutes = $script.IntervalMinutes
+                # the script that should be executed, retrieved from the database
+                $executescript = $result.ExecuteScript
 
-                # compare values and handle processing (greater than or equal to comparison)
-                if ($minuteselapsed -ge $intervalminutes) {
+                # check that the script file exists
+                if (Test-Path $scriptname -PathType Leaf) {
+                    # check when the script was last run and compare to the pre-defined value for how many minutes should have elapsed
+                    # this will avoid that say, a script that should run Monthly is run multiple times during the month
+                    # the COALESCE function will either return the value of the most recent RecordCreated column for that server OR the value 600,000 (which is more than 1 year in minutes)
+                    $sql = "SELECT COALESCE(DATEDIFF(N, MAX([RecordCreated]), CURRENT_TIMESTAMP), 600000) AS [MinutesElapsed] FROM $($ProfileName).$($tablename) WHERE [ServerName] = '$($ServerName)';"
+                    $result = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $sql
+                    $minuteselapsed = $result.MinutesElapsed
+                    $result = $null
 
-                    # run any script marked for pre-execution
-                    $preexecutescript = $script.PreExecuteScript
-                    # NOTE: if NOT IsNullOrEmpty...
-                    if (![string]::IsNullOrEmpty($preexecutescript)) {
-                        # replace the parameter with the server name
-                        $preexecutescript = $preexecutescript -f $ServerName
-                        # execute the query against the monitoring database
-                        $preexecuteresult = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $preexecutescript
+                    # compare values and handle processing (greater than or equal to comparison)
+                    if ($minuteselapsed -ge $intervalminutes) {
+
+                        # run any script marked for pre-execution
+                        $preexecutescript = $script.PreExecuteScript
+                        # NOTE: if NOT IsNullOrEmpty...
+                        if (![string]::IsNullOrEmpty($preexecutescript)) {
+                            # replace the parameter with the server name
+                            $preexecutescript = $preexecutescript -f $ServerName
+                            # execute the query against the monitoring database
+                            $preexecuteresult = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $preexecutescript
+                        }
+
+                        "{0} : Running script:    {1}" -f $(Get-Date -Format "HH:mm:ss"), $scriptname
+                        # run the script retrieved from the database, otherwise load it from the file
+                        if ([string]::IsNullOrEmpty($runscript)) {
+                            $sql = Get-Content -Path $scriptname -Raw
+                        }
+                        else {
+                            $sql = $runscript
+                        }
+                        # replace the script parameter with the result obtained
+                        # NOTE: if NOT IsNullOrEmpty...
+                        if (![string]::IsNullOrEmpty($preexecuteresult)) {
+                            $sql = $sql -f $preexecuteresult.Output
+                        }
+                        # run and store the output in a data table variable
+                        try {
+                            $result = Invoke-Sqlcmd2 -ServerInstance $InstanceName -Database master -Query $sql.ToString()
+                        }
+                        catch {
+                            $ErrorMessage = $_.Exception.Message
+                            Write-Warning $ErrorMessage
+                        }
+                        
+                        # check if the connection test was successful
+                        if (![string]::IsNullOrEmpty($runscript)) {
+                            $dt = $result | Out-DataTable
+                            $dtRowCount = $dt.Rows.Count
+
+                            if ($dtRowCount -gt 0) {
+                                # workaround to remove excess columns added when converting to data table - start
+                                $dt.Columns.Remove("RowError")
+                                $dt.Columns.Remove("RowState")
+                                $dt.Columns.Remove("Table")
+                                $dt.Columns.Remove("ItemArray")
+                                $dt.Columns.Remove("HasErrors")
+                                # workaround to remove excess columns added when converting to data table - start
+                            }
+
+                            # update the status for older data
+                            $sql = "UPDATE $($ProfileName).$($tablename) SET [RecordStatus] = 'H' WHERE [ServerName] = '$($ServerName)' AND [RecordStatus] = 'A';"
+                            Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $sql
+
+                            # write data extraced from remote server to central table
+                            if ($dtRowCount -gt 0) {
+                                Write-DataTable -Data $dt -ServerInstance $ServerInstance -Database $Database -TableName "$($ProfileName).$($tablename)"
+                            }
+                        }
+                        # clean up
+                        $runscript = $null
+                        $executescript = $null
+                        $dt = $null
+                        $dtRowCount = 0
+                        $preexecutescript = ""
+                        $preexecuteresult = $null
                     }
-
-                    "{0} : Running script:    {1}" -f $(Get-Date -Format "HH:mm:ss"), $scriptname
-                    # run the script retrieved from the database, otherwise load it from the file
-                    if ([string]::IsNullOrEmpty($runscript)) {
-                        $sql = Get-Content -Path $scriptname -Raw
-                    }
+                    # script has been executed against the current server in the past N minutes
                     else {
-                        $sql = $runscript
-                    }
-                    # replace the script parameter with the result obtained
-                    # NOTE: if NOT IsNullOrEmpty...
-                    if (![string]::IsNullOrEmpty($preexecuteresult)) {
-                        $sql = $sql -f $preexecuteresult.Output
-                    }
-                    # run and store the output in a data table variable
-                    $result = Invoke-Sqlcmd2 -ServerInstance $InstanceName -Database master -Query $sql.ToString()
-                    $dt = $result | Out-DataTable
-                    $dtRowCount = $dt.Rows.Count
+                        $ts =  [timespan]::fromminutes($minuteselapsed)
+                        $age = New-Object DateTime -ArgumentList $ts.Ticks
 
-                    if ($dtRowCount -gt 0) {
-                        # workaround to remove excess columns added when converting to data table - start
-                        $dt.Columns.Remove("RowError")
-                        $dt.Columns.Remove("RowState")
-                        $dt.Columns.Remove("Table")
-                        $dt.Columns.Remove("ItemArray")
-                        $dt.Columns.Remove("HasErrors")
-                        # workaround to remove excess columns added when converting to data table - start
-                    }
+                        $msg = "" 
 
-                    # update the status for older data
-                    $sql = "UPDATE $($ProfileName).$($tablename) SET [RecordStatus] = 'H' WHERE [ServerName] = '$($ServerName)' AND [RecordStatus] = 'A';"
-                    Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $sql
+                        if ($($age.Year-1) -gt 0) { $msg += " " + $($age.Year-1).ToString() + " Years" }
+                        if ($($age.Month-1) -gt 0) { $msg += " " + $($age.Month-1).ToString() + " Months" }
+                        if ($($age.Day-1) -gt 0) { $msg += " " + $($age.Day-1).ToString() + " days" }
+                        if ($($age.Hour) -gt 0) { $msg += " " + $($age.Hour).ToString() + " hours" }
+                        if ($($age.Minute) -gt 0) { $msg += " " + $($age.Minute).ToString() + " minutes" }
+                        #if ($($age.second) -gt 0) { $msg += " " + $($age.second).ToString() + " seconds" }
 
-                    # write data extraced from remote server to central table
-                    if ($dtRowCount -gt 0) {
-                        Write-DataTable -Data $dt -ServerInstance $ServerInstance -Database $Database -TableName "$($ProfileName).$($tablename)"
+                        $msg += " ago"
+
+                        "{0} : Script {1} has already been run$msg." -f $(Get-Date -Format "HH:mm:ss"), $scriptname
                     }
-                    # clean up
-                    $runscript = $null
-                    $executescript = $null
-                    $dt = $null
-                    $dtRowCount = 0
-                    $preexecutescript = ""
-                    $preexecuteresult = $null
                 }
-                # script has been executed against the current server in the past N minutes
+                # script file does not exist
                 else {
-                    $ts =  [timespan]::fromminutes($minuteselapsed)
-                    $age = New-Object DateTime -ArgumentList $ts.Ticks
-
-                    $msg = "" 
-
-                    if ($($age.Year-1) -gt 0) { $msg += " " + $($age.Year-1).ToString() + " Years" }
-                    if ($($age.Month-1) -gt 0) { $msg += " " + $($age.Month-1).ToString() + " Months" }
-                    if ($($age.Day-1) -gt 0) { $msg += " " + $($age.Day-1).ToString() + " days" }
-                    if ($($age.Hour) -gt 0) { $msg += " " + $($age.Hour).ToString() + " hours" }
-                    if ($($age.Minute) -gt 0) { $msg += " " + $($age.Minute).ToString() + " minutes" }
-                    #if ($($age.second) -gt 0) { $msg += " " + $($age.second).ToString() + " seconds" }
-
-                    $msg += " ago"
-
-                    "{0} : Script {1} has already been run$msg." -f $(Get-Date -Format "HH:mm:ss"), $scriptname
+                    "{0} : Script {1} not found." -f $(Get-Date -Format "HH:mm:ss"), $scriptname
                 }
+                $scriptname = $null
+                $tablename = $null
+                $intervalminutes = $null
             }
-            # script file does not exist
-            else {
-                "{0} : Script {1} not found." -f $(Get-Date -Format "HH:mm:ss"), $scriptname
-            }
-            $scriptname = $null
-            $tablename = $null
-            $intervalminutes = $null
         }
         "{0} : Completed server:  {1}" -f $(Get-Date -Format "HH:mm:ss"), $ServerName
         "{0} : ------------------------------ " -f $(Get-Date -Format "HH:mm:ss")
@@ -150,4 +181,47 @@ Function Get-ServerInfo() {
     }
     "{0} : Done" -f $(Get-Date -Format "HH:mm:ss")
     "{0} : ============================== " -f $(Get-Date -Format "HH:mm:ss")
+}
+
+
+function Test-Port($hostname, $port) {
+    # This works no matter in which form we get $host - hostname or ip address
+    try {
+        $ip = [System.Net.Dns]::GetHostAddresses($hostname) | 
+            Select-Object IPAddressToString -expandproperty  IPAddressToString
+        if ($ip.GetType().Name -eq "Object[]") {
+            #If we have several ip's for that address, let's take first one
+            $ip = $ip[0]
+        }
+    } 
+    catch {
+        #Write-Host "$hostname could be the incorrect Hostname or IP Address"
+        Return $False
+    }
+    $t = New-Object Net.Sockets.TcpClient
+    # We use Try\Catch to remove exception info from console if we can't connect
+    try {$t.Connect($ip,$port)} catch {}
+
+    if($t.Connected) {
+        $t.Close()
+        <#
+        $object = [pscustomobject] @{
+                        Hostname = $hostname
+                        IP = $IP
+                        TCPPort = $port
+                        GetResponse = $True }
+        Write-Output $object
+        #>
+        Return $True
+    }
+    else {
+        <#
+        $object = [pscustomobject] @{
+                        Computername = $IP
+                        TCPPort = $port
+                        GetResponse = $False }
+        Write-Output $object
+        #>
+        Return $False
+    }
 }
